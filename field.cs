cd5a72b478e37ed81bbb879aea2ac586bf7f84f7
a-kostousova <38812110+a-kostousova@users.noreply.github.com>
2018-04-28 19:48:20 +0500

    Поправила стиль, задала вопросы, обрати внимание

--- field.cs
+++ field.cs
@@ -1,23 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace mahjong
+namespace Mahjong
 {
-    class field
+    class Field
     {
-        private int width { get { return columns.GetLength(0); } }
-        private int length { get { return columns.GetLength(1); } }
-        private Stack<Dice>[,] columns;
+        private int Width { get { return columns.GetLength(0); } }
+        private int Length { get { return columns.GetLength(1); } }
+        private Stack<Dice>[,] Columns; //что это (я правда не очень понимаю)?
 
-        public field(int width, int length)
+        public Field(int width, int length) //нормально, что имя совпадает с именем класса?
         {
             columns = new Stack<Dice>[width, length];
         }
 
-        public void fuul(int count)
+        public void fuul(int count) //что тут должно быть?
         {
             for (var i = 0; i < count; i++)
 
