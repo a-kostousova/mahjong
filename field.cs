f9fa55ee540f1d5b03dcf6c99d9811a31f961bee
MaiklM <38812453+MaiklM@users.noreply.github.com>
2018-04-29 14:04:22 +0500

    Executable - меняет поле Executable у Dice

--- field.cs
+++ field.cs
@@ -25,13 +25,49 @@ namespace Mahjong
             {
                 var dice = new Dice().Generate();
                 var coordinate = GenerateCoordinate();
-                columns[coordinate.Item1, coordinate.Item2].Push(dice);
+                Push(coordinate, dice);
+                Executable(coordinate, false);
                 var coordinate2 = GenerateCoordinate();
                 //убеждаюсь, что новая координата не совпала с первой
                 while (coordinate == coordinate2)
                     coordinate2 = GenerateCoordinate();
-                columns[coordinate2.Item1, coordinate2.Item2].Push(dice);
+                Push(coordinate, dice);
+                Executable(coordinate2, false);
             }
         }
+        
+        void Executable(int x, int y)
+        {
+            if (x == 0 || x == width || y == 0 || y == length) columns[x, y].Peek().Executable = true;
+            else if (columns[x - 1, y].Count < columns[x, y].Count ||
+                     columns[x + 1, y].Count < columns[x, y].Count ||
+                     columns[x, y - 1].Count < columns[x, y].Count ||
+                     columns[x, y + 1].Count < columns[x, y].Count)
+                     columns[x, y].Peek().Executable = true;
+        }
+
+        void Executable(Tuple<int, int> coordinate, bool isNeighbor)
+        {
+            var x = coordinate.Item1;
+            var y = coordinate.Item2;
+            Executable(x, y);
+            if (!isNeighbor)
+            {
+                if (x != 0) Executable(Tuple.Create(x - 1, y), true);
+                if (x != width - 1) Executable(Tuple.Create(x + 1, y), true);
+                if (y != 0) Executable(Tuple.Create(x, y - 1), true);
+                if (y != length - 1) Executable(Tuple.Create(x, y + 1), true);
+            }
+        }
+
+        public void Delete(Tuple<int, int> coordinate)
+        {
+            columns[coordinate.Item1, coordinate.Item2].Pop();
+            Executable(coordinate, false);
+        }
+
+        public void Push(Tuple<int, int> coordinate, Dice value) => this[coordinate].Push(value);
+
+        private Stack<Dice> this[Tuple<int, int> coordinate] { get => columns[coordinate.Item1, coordinate.Item2]; }
     }
 }
