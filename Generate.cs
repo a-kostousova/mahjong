bebb44911935b2cba20c2bff54dff199ee120e6d
MaiklM <38812453+MaiklM@users.noreply.github.com>
2018-05-26 09:20:25 +0500

    добавил новый класс генерации

---
+++ Generate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahjong
+{
+    static class Generate
+    {
+        private static Random Random = new Random();
+
+        public static Tuple<int, int> Coordinate(int length, int width) => Tuple.Create(Random.Next(length), Random.Next(width));
+
+        public static Dice Dice()
+        {
+            var colorCount = Enum.GetValues(typeof(Colours)).Length;
+            var picturesCount = Enum.GetValues(typeof(Pictures)).Length;
+            return new Dice(
+                (Colours)Random.Next(colorCount),
+                (Pictures)Random.Next(picturesCount));
+        }
+    }
+}
