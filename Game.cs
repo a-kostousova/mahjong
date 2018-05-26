92a188afab7592539ee923a1152f6a9fef03e83b
MaiklM <38812453+MaiklM@users.noreply.github.com>
2018-05-26 09:19:02 +0500

    исправил ChosenDice, вспомогательный метод Takely

--- Game.cs
+++ Game.cs
@@ -11,13 +11,18 @@ namespace Mahjong
     {
         private Field Field;
         private Tuple<int, int> DicePointer;
-        private Tuple<Dice, Dice> ChosenDices;
+        private Tuple<int, int> ChosenDices;
+        
+        public Game(Field field) => Field = field;
         
         public Game(int length, int width, int count)
         {
             Field = new Field(length, width);
             Field.FillField(count);
         }
+        
+        private bool Takely(Tuple<int, int> coordinate) => Field[coordinate].Count != 0 &&
+               (Field.Extreme(coordinate) || Field.Higher(coordinate));
 
         public void PressKey(Keys key)
         {
@@ -44,25 +49,27 @@ namespace Mahjong
         private void MoveDicePointer(int dx, int dy) => DicePointer = Tuple.Create(
                 SumMod(DicePointer.Item1, dx, Field.Length), 
                 SumMod(DicePointer.Item2, dy, Field.Width));
+        
         private int SumMod(int x, int y, int mod) => (x + y + mod) % mod;
 
         private void DeleteDices()
         {
             Field.Delete(DicePointer);
             Field.Delete(ChosenDice.Item2);
-            ChosenDice = Tuple.Create(false, ChosenDice.Item2);
+            ChosenDice = null;
             if (Field.PairCount == 0) Console.WriteLine("Game Over, You Win");
         }
         
         private void ChooseDice()
         {
-            if (ChosenDice.Item1)
-            {
-                if (ChosenDice.Item2 == DicePointer) ChosenDice = Tuple.Create(false, ChosenDice.Item2);
-                else if (Field[ChosenDice.Item2].Equals(Field[DicePointer])) DeleteDices();
-                    else ChosenDice = Tuple.Create(false, ChosenDice.Item2);
-            }
-            else ChosenDice = Tuple.Create(true, DicePointer);
+            if (Takely(DicePointer))
+                if (ChosenDice.Item1)
+                {
+                    if (ChosenDice == DicePointer) ChosenDice = null;
+                    else if (Field[ChosenDice].Peek().Equals(Field[DicePointer].Peec())) DeleteDices();
+                        else ChosenDice = null;
+                }
+                else ChosenDice = DicePointer;
         }
     }
 }
